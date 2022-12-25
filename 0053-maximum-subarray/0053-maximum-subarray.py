ed08dc5abb57703a2b70ad2612b5979c32a53379
minchan kim <43123236+mckim123@users.noreply.github.com>
2022-12-25 21:13:14 +0900

    Time: 702 ms (97.78%), Space: 27.8 MB (79.33%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.py
+++ 0053-maximum-subarray/0053-maximum-subarray.py
@@ -1,5 +1,7 @@
 class Solution:
     def maxSubArray(self, nums: List[int]) -> int:
+        if len(nums) == 1:
+            return nums[0]
         prefix = 0
         m = 0
         answer = -10**5

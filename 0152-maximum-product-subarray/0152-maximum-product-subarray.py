0cd099ec32d4adc23684b93afeb63c9a88a5ea80
minchan kim <43123236+mckim123@users.noreply.github.com>
2022-12-25 21:27:37 +0900

    Time: 72 ms (99.73%), Space: 14.4 MB (79.94%) - LeetHub

--- 0152-maximum-product-subarray/0152-maximum-product-subarray.py
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.py
@@ -2,28 +2,28 @@ class Solution:
     def maxProduct(self, nums: List[int]) -> int:
         answer = 0 if 0 in nums else -10
         while(0 in nums):
-            answer = max(answer, self.findMaxWithoutZero(nums[:nums.index(0)]))
+            answer = max(answer, findMaxWithoutZero(nums[:nums.index(0)]))
             nums = nums[nums.index(0)+1:]
-        answer = max(answer, self.findMaxWithoutZero(nums))
+        answer = max(answer, findMaxWithoutZero(nums))
         return answer
     
-    def findMaxWithoutZero(self, nums):
-        if len(nums) == 0:
-            return -10
-        elif len(nums) == 1:
-            return nums[0]
-        elif len(nums) == 2:
-            return max(nums[0], nums[1], nums[0]*nums[1])
-        else:
-            max_prod = 1
-            prod = 1
-            for num in nums:
-                prod *= num
-                if prod > max_prod:
-                    max_prod = prod
-            prod = 1
-            for num in nums[::-1]:
-                prod *= num
-                if prod > max_prod:
-                    max_prod = prod
-            return max_prod
+def findMaxWithoutZero(nums):
+    if len(nums) == 0:
+        return -10
+    elif len(nums) == 1:
+        return nums[0]
+    elif len(nums) == 2:
+        return max(nums[0], nums[1], nums[0]*nums[1])
+    else:
+        max_prod = 1
+        prod = 1
+        for num in nums:
+            prod *= num
+            if prod > max_prod:
+                max_prod = prod
+        prod = 1
+        for num in nums[::-1]:
+            prod *= num
+            if prod > max_prod:
+                max_prod = prod
+        return max_prod

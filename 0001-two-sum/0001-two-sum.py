587390c31ff6371f26c0d5ed5b32b3d2f12ca7a4
minchan kim <43123236+mckim123@users.noreply.github.com>
2022-12-25 18:14:14 +0900

    Time: 52 ms (99.57%), Space: 15.2 MB (52.61%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,22 +1,8 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        sorted_nums = sorted(nums)
-        n = len(nums)
-        small, large = 0, n-1
-        sum = sorted_nums[small] + sorted_nums[large]
-        while sum != target:
-            if sum > target:
-                large -= 1
-            else:
-                small += 1
-            sum = sorted_nums[small] + sorted_nums[large]
-        small_index = nums.index(sorted_nums[small])
-        large_index = nums.index(sorted_nums[large])
-        if small_index > large_index:
-            return [large_index, small_index]
-        elif small_index == large_index:
-            nums[small_index] += 1
-            return [small_index, nums.index(sorted_nums[small])]
-        else:
-            return [small_index, large_index]
-        
+        hashmap = {}
+        for i in range(len(nums)):
+            comp = target - nums[i]
+            if comp in hashmap:
+                return [i, hashmap[comp]]
+            hashmap[nums[i]] = i

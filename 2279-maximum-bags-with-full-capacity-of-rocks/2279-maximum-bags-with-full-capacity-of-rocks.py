dc42b21602c655e671ce9780e016b24842cc20c6
minchan kim <43123236+mckim123@users.noreply.github.com>
2022-12-28 01:38:58 +0900

    Time: 893 ms (99.67%), Space: 22 MB (96.05%) - LeetHub

--- 2279-maximum-bags-with-full-capacity-of-rocks/2279-maximum-bags-with-full-capacity-of-rocks.py
+++ 2279-maximum-bags-with-full-capacity-of-rocks/2279-maximum-bags-with-full-capacity-of-rocks.py
@@ -2,12 +2,11 @@ import heapq
 
 class Solution:
     def maximumBags(self, capacity: List[int], rocks: List[int], additionalRocks: int) -> int:
-        n = len(capacity)
-        for i in range(n):
+        for i in range(len(capacity)):
             capacity[i] -= rocks[i]
         capacity.sort(reverse = True)
         answer = 0
-        for _ in range(n):
+        while(capacity):
             additionalRocks -= capacity.pop()
             if 0 > additionalRocks:
                 return answer

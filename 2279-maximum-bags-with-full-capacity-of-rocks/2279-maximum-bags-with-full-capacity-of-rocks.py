b94cbeb183d95fb0f17a8ca94ce1c20ec4f856ad
minchan kim <43123236+mckim123@users.noreply.github.com>
2022-12-28 01:32:38 +0900

    Time: 966 ms (87.17%), Space: 22.3 MB (32.89%) - LeetHub

---
+++ 2279-maximum-bags-with-full-capacity-of-rocks/2279-maximum-bags-with-full-capacity-of-rocks.py
@@ -0,0 +1,14 @@
+import heapq
+
+class Solution:
+    def maximumBags(self, capacity: List[int], rocks: List[int], additionalRocks: int) -> int:
+        requires = [x-y for x,y in zip(capacity, rocks)]
+        heapq.heapify(requires)
+        print(requires)
+        answer = 0
+        while(requires):
+            additionalRocks -= heapq.heappop(requires)
+            if additionalRocks < 0:
+                return answer
+            answer += 1
+        return answer

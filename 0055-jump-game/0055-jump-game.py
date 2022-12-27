b5f57d94e8d79ccd219342bc8317c5d01b43d1b9
minchan kim <43123236+mckim123@users.noreply.github.com>
2022-12-28 01:48:19 +0900

    Time: 472 ms (97.04%), Space: 15.2 MB (82.62%) - LeetHub

--- 0055-jump-game/0055-jump-game.py
+++ 0055-jump-game/0055-jump-game.py
@@ -2,11 +2,12 @@ class Solution:
     def canJump(self, nums: List[int]) -> bool:
         pos = 0
         reachableIndex = 0
+        n = len(nums)
         while pos <= reachableIndex:
             temp = nums[pos] + pos
             if temp > reachableIndex:
                 reachableIndex = temp
-                if temp >= len(nums):
+                if temp >= n:
                     return True
             pos += 1
-        return reachableIndex+1 >= len(nums)
+        return reachableIndex+1 >= n

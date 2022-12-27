d83a646b36c05421cc8c1fec031c777674812c6b
minchan kim <43123236+mckim123@users.noreply.github.com>
2022-12-28 01:32:46 +0900

    Time: 985 ms (81.91%), Space: 22.1 MB (87.83%) - LeetHub

--- 2279-maximum-bags-with-full-capacity-of-rocks/2279-maximum-bags-with-full-capacity-of-rocks.py
+++ 2279-maximum-bags-with-full-capacity-of-rocks/2279-maximum-bags-with-full-capacity-of-rocks.py
@@ -4,7 +4,6 @@ class Solution:
     def maximumBags(self, capacity: List[int], rocks: List[int], additionalRocks: int) -> int:
         requires = [x-y for x,y in zip(capacity, rocks)]
         heapq.heapify(requires)
-        print(requires)
         answer = 0
         while(requires):
             additionalRocks -= heapq.heappop(requires)

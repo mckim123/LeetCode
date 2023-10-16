2999ab501cd8a4b3c292217aee93c86540c517b6
minchan kim <43123236+mckim123@users.noreply.github.com>
2023-10-16 23:11:55 +0900

    Time: 590 ms (92.07%), Space: 35.6 MB (63.40%) - LeetHub

--- 2659-make-array-empty/2659-make-array-empty.py
+++ 2659-make-array-empty/2659-make-array-empty.py
@@ -3,17 +3,13 @@ class Solution:
         n = len(nums)
         pos = {v:i for i, v in enumerate(nums)}
         ans = 0
-        reps = 0
-        idx = 0
+        reps, idx = 0, 0
         
         for num in sorted(nums):
             if pos[num] < idx:
-                idx = pos[num]
                 reps += 1
-                ans += reps
-            else:
-                idx = pos[num]
-                ans += reps    
+            idx = pos[num]
+            ans += reps    
         
         return ans + n
     

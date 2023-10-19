a5ccbfc147683fc89cecf541d11fbfa9b83b57e0
minchan kim <43123236+mckim123@users.noreply.github.com>
2023-10-19 22:51:13 +0900

    Time: 37 ms (70.96%), Space: 16.2 MB (54.52%) - LeetHub

--- 0844-backspace-string-compare/0844-backspace-string-compare.py
+++ 0844-backspace-string-compare/0844-backspace-string-compare.py
@@ -1,12 +1,24 @@
 class Solution:
     def backspaceCompare(self, s: str, t: str) -> bool:
-        def convert(s):
-            char_list = []
-            for ch in s:
+        end1, end2 = len(s), len(t)
+
+        def get_last_valid_character(string, end):
+            count = 0
+            while end:
+                ch = string[end-1]
+                end -= 1
                 if ch == "#":
-                    if char_list:
-                        char_list.pop()
+                    count += 1
+                elif count:
+                    count -= 1
                 else:
-                    char_list.append(ch)
-            return char_list
-        return convert(s) == convert(t)
+                    return (ch, end)
+            return ("", end)
+        
+        while end1 or end2:
+            ch1, end1 = get_last_valid_character(s, end1)
+            ch2, end2 = get_last_valid_character(t, end2)
+            if ch1 != ch2:
+                return False
+        return True
+

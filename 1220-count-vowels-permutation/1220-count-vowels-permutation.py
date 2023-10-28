5725af439d51286d91385334de4fde9517829db8
minchan kim <43123236+mckim123@users.noreply.github.com>
2023-10-28 20:12:09 +0900

    Time: 45 ms (100.00%), Space: 16.1 MB (95.14%) - LeetHub

--- 1220-count-vowels-permutation/1220-count-vowels-permutation.py
+++ 1220-count-vowels-permutation/1220-count-vowels-permutation.py
@@ -2,15 +2,40 @@ class Solution:
     def countVowelPermutation(self, n: int) -> int:
         MOD = int(1e9) + 7
         
-        prev = [1, 1, 1, 1, 1]
+        if n==1 :
+            return 5
         
-        for _ in range(n-1):
-            curr = []   # a, e, i, o, u
-            curr.append(prev[1])
-            curr.append((prev[0] + prev[2]) % MOD)
-            curr.append((prev[0] + prev[1] + prev[3] + prev[4]) % MOD)
-            curr.append((prev[2] + prev[4]) % MOD)
-            curr.append(prev[0])
-            prev = curr
+        def mult(A, B):
+            return [[sum(A[i][k] * B[k][j] for k in range(5)) % MOD for j in range(5)] for i in range(5)]
+
+        def mat_pow(A, p):
+            if p == 1:
+                return A
+            if p % 2:
+                return mult(A, mat_pow(A, p-1))
+            half_pow = mat_pow(A, p // 2)
+            return mult(half_pow, half_pow)
+        
+        mat = [
+            [0, 1, 0, 0, 0],
+            [1, 0, 1, 0, 0],
+            [1, 1, 0, 1, 1],
+            [0, 0, 1, 0, 1],
+            [1, 0, 0, 0, 0]
+        ]
+        
+        return sum(sum(row) for row in mat_pow(mat, n-1)) % MOD
+    
+#         prev = [1, 1, 1, 1, 1]
+        
+#         for _ in range(n-1):
+#             curr = []   # a, e, i, o, u
+#             curr.append(prev[1])
+#             curr.append((prev[0] + prev[2]) % MOD)
+#             curr.append((prev[0] + prev[1] + prev[3] + prev[4]) % MOD)
+#             curr.append((prev[2] + prev[4]) % MOD)
+#             curr.append(prev[0])
+#             prev = curr
             
-        return sum(prev) % MOD
+#         return sum(prev) % MOD
+        

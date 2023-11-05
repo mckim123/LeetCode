6acd54be62693d735ca891c085c2e8204dfe6350
minchan kim <43123236+mckim123@users.noreply.github.com>
2023-11-05 12:18:59 +0900

    Time: 517 ms (80.98%), Space: 29.4 MB (96.74%) - LeetHub

--- 1535-find-the-winner-of-an-array-game/1535-find-the-winner-of-an-array-game.py
+++ 1535-find-the-winner-of-an-array-game/1535-find-the-winner-of-an-array-game.py
@@ -1,10 +1,9 @@
 class Solution:
     def getWinner(self, arr: List[int], k: int) -> int:
         prev_winner = arr[0]
-        streak = 0
-        for i in range(1, len(arr)):
-            curr = arr[i]
-            if prev_winner > curr:
+        streak = -1
+        for curr in arr:
+            if prev_winner >= curr:
                 streak += 1
             else:
                 prev_winner = curr

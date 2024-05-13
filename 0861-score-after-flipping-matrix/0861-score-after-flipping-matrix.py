20c61b4179c0d65ecbb365150befb7ad3c37e0a3
minchan kim <43123236+mckim123@users.noreply.github.com>
2024-05-13 21:42:53 +0900

    Time: 46 ms (23.28%), Space: 16.5 MB (92.67%) - LeetHub

---
+++ 0861-score-after-flipping-matrix/0861-score-after-flipping-matrix.py
@@ -0,0 +1,16 @@
+class Solution:
+    def matrixScore(self, grid: List[List[int]]) -> int:
+        m, n = len(grid), len(grid[0])
+        counter = [0] * n
+        for row in grid:
+            k = row[0]
+            for i, num in enumerate(row):
+                if k == num:
+                    counter[i] += 1
+        ans = (1 << (n-1)) * m
+        
+        for i in range(1, n):
+            c = counter[i]
+            ans += (1 << (n-1-i)) * max(c, m-c)
+        
+        return ans

e138bdfe36891cadec30f1bd580176a5f16ed6aa
minchan kim <43123236+mckim123@users.noreply.github.com>
2024-08-18 15:07:19 +0900

    Time: 113 ms (47.84%), Space: 16.6 MB (71.48%) - LeetHub

--- 0264-ugly-number-ii/0264-ugly-number-ii.py
+++ 0264-ugly-number-ii/0264-ugly-number-ii.py
@@ -6,15 +6,13 @@ class Solution:
         cur, cur1, cur2, cur3 = 0, 0, 0, 0
         for _ in range(n-1):
             cur = heappop(h)
-            cur1 = cur * 2
+            cur1, cur2, cur3 = cur * 2, cur * 3, cur * 5
             if cur1 not in s:
                 heappush(h, cur1)
                 s.add(cur1)
-            cur2 = cur * 3
             if cur2 not in s:
                 heappush(h, cur2)
                 s.add(cur2)
-            cur3 = cur * 5
             if cur3 not in s:
                 heappush(h, cur3)
                 s.add(cur3)
